3a606caea2c87cac178bdb60ac4ef3307846f7fa
Kovidh <kovidhvsbhati@gmail.com>
2025-09-19 00:07:38 +0530

    client feature-multiple arguments for create todo

--- client/client.js
+++ client/client.js
@@ -10,30 +10,51 @@ const todoPackage = grpcObject.todoPackage;
 const client = new todoPackage.Todo(process.env.SERVER_PORT, grpc.credentials.createInsecure());
 const text = process.argv[2];
 
+const input = process.argv;
+
+if (input.length == 3 && input[2] == "read") {
+	client.readTodos({}, (err, response) => {
+		// console.log("response read ", JSON.stringify(response));
+		if (response.items)
+			response.items.forEach(i => { console.log(i.text) });
+	});
+
+} else if (input.length >= 4 && input[2] == "create") {
+
+	for (let i = 3; i < input.length; i++) {
+		const todo = input[i];
+		client.createTodo({
+			"id": -1,
+			"text": todo
+		}, (err, response) => {
+			console.log("response create " + JSON.stringify(response));
+		});
+	}
+
+} else {
+	console.log("wrong parameters!");
+}
+
 // remote call to createTodo. Single request response call
-client.createTodo({
-	"id": -1,
-	"text": text
-}, (err, response)=>{
-	console.log("response create " + JSON.stringify(response));
-});
+// client.createTodo({
+// 	"id": -1,
+// 	"text": text
+// }, (err, response) => {
+// 	console.log("response create " + JSON.stringify(response));
+// });
 
 // remote call to readTodos. Single request response call
-client.readTodos({}, (err, response)=>{
-    // console.log("response read ", JSON.stringify(response));
-	if(response.items)
-    	response.items.forEach(i => {console.log(i.text)});
-});
+
 
 
 // remote call to readTodosStream. Receive stream of data from server
-const call = client.readTodosStream();
+// const call = client.readTodosStream();
 
-call.on("data", item => { // receiving data stream from server -- on call.write(item) from server
-	console.log("data item received from server: "+JSON.stringify(item));
-})
+// call.on("data", item => { // receiving data stream from server -- on call.write(item) from server
+// 	console.log("data item received from server: " + JSON.stringify(item));
+// })
 
-call.on("end", e => console.log("server done!")); // on call.end() from server
+// call.on("end", e => console.log("server done!")); // on call.end() from server
 
 /*
  * Client connects to server
